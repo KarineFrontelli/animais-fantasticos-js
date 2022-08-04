31293fa7dc87ec4a5f6867e703456360c9b48e39
KarineFrontelli <you@example.comkarinefrontellidossantos@gmail.com>
2022-08-04 18:29:38 -0300

    scroll suave no Link Interno

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ initTabNav();
 
 function initAccordion() {
   const accordionList = document.querySelectorAll(".js-accordion dt");
-  const activeClass = 'ativo';
+  const activeClass = "ativo";
 
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
@@ -39,3 +39,28 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    /*forma alternativa
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior:'smooth',
+    })*/
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
